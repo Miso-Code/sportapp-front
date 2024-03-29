3fcb6f510c3e6a1a23578002ea46f9f04c0a830b
ocralo <r.escobarl@uniandes.edu.co>
2024-03-28 23:55:03 -0500

    feat: add init config in web app PSA-152

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,14 @@
 import { useTranslation } from 'react-i18next'
 import './App.scss'
+import Button from '@mui/material/Button'
 
 function App() {
 	const { t } = useTranslation()
 
 	return (
 		<>
-			<h1>{t('app.name')}</h1>
+			<h1 className='text-9xl'>{t('app.name')}</h1>
+			<Button variant='contained'>Hello world</Button>
 		</>
 	)
 }
