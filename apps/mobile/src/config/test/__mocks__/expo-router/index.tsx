abbf167ae5ea2029d554d4afce9c1bd946a98299
Erik Loaiza <ef.loaiza@uniandes.edu.co>
2024-04-06 16:14:28 -0500

    feat: added user login PSA-28

---
+++ apps/mobile/src/config/test/__mocks__/expo-router/index.tsx
@@ -0,0 +1,10 @@
+const Tabs = ({ children }) => <>{children}</>
+
+Tabs.Screen = ({ options }) => (
+	<div>
+		<h1>{options.title}</h1>
+		<>{options.tabBarIcon({ color: 'blue' })}</>
+	</div>
+)
+
+export { Tabs }
---
+++ apps/mobile/src/config/test/__mocks__/expo-router/stack.tsx
@@ -0,0 +1,5 @@
+const Stack = ({ children }) => <>{children}</>
+
+Stack.Screen = () => 'Mock Screen'
+
+export { Stack }
