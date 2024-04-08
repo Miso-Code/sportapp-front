dde974520b2f47427c6a73763922a76afd9334f9
ocralo <r.escobarl@uniandes.edu.co>
2024-04-08 18:14:44 -0500

    feat: update routes PSA-158

---
+++ apps/web/src/router/public/Layout/index.tsx
@@ -0,0 +1,13 @@
+import { Props } from '@/router/public/Layout/interfaces/'
+import { CircularProgress } from '@mui/material'
+import { Suspense } from 'react'
+
+export default function LayoutPublic({ children }: Props) {
+	return (
+		<>
+			<Suspense fallback={<CircularProgress size='large' />}>
+				{children}
+			</Suspense>
+		</>
+	)
+}
