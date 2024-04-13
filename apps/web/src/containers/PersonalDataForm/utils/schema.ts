25b5b9f974a598c8cfba49668c0916334406029a
ocralo <r.escobarl@uniandes.edu.co>
2024-04-13 10:05:17 -0500

    feat: update schema PSA-169

--- apps/web/src/containers/PersonalDataForm/utils/schema.ts
+++ apps/web/src/containers/PersonalDataForm/utils/schema.ts
@@ -1,25 +1,15 @@
-import defaultRegisterSchema from '@/containers/Register/Default/utils/schema'
+import fullRegisterSchema from '@/containers/Register/Full/utils/schema'
 import 'config/lang/form.ts'
 import { InferType, object, string } from 'yup'
 
-const fullRegisterSchema = object().shape({
-	password: string(),
-	documentType: string().required(),
-	documentNumber: string().required(),
-	nationality: object().shape({
-		country: string().required(),
-		city: string().required()
-	}),
-	residence: object().shape({
-		country: string().required(),
-		city: string().required(),
-		lengthOfStay: string().required()
-	}),
-	gender: string().required(),
-	birthday: string().required()
+const personalDataDefaultSchema = object().shape({
+	password: string().notRequired(),
+	name: string().required(),
+	lastName: string().required(),
+	email: string().email().required()
 })
 
-const personalDataSchema = defaultRegisterSchema.concat(fullRegisterSchema)
+const personalDataSchema = fullRegisterSchema.concat(personalDataDefaultSchema)
 
 export default personalDataSchema
 
