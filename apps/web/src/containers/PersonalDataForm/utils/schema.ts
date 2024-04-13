8d3080ce027088a6c9063ab4206facbc5df20e6f
ocralo <r.escobarl@uniandes.edu.co>
2024-04-13 09:49:55 -0500

    feat: update personal data form PSA-169

--- apps/web/src/containers/PersonalDataForm/utils/schema.ts
+++ apps/web/src/containers/PersonalDataForm/utils/schema.ts
@@ -1,7 +1,23 @@
 import defaultRegisterSchema from '@/containers/Register/Default/utils/schema'
-import fullRegisterSchema from '@/containers/Register/Full/utils/schema'
 import 'config/lang/form.ts'
-import { InferType } from 'yup'
+import { InferType, object, string } from 'yup'
+
+const fullRegisterSchema = object().shape({
+	password: string(),
+	documentType: string().required(),
+	documentNumber: string().required(),
+	nationality: object().shape({
+		country: string().required(),
+		city: string().required()
+	}),
+	residence: object().shape({
+		country: string().required(),
+		city: string().required(),
+		lengthOfStay: string().required()
+	}),
+	gender: string().required(),
+	birthday: string().required()
+})
 
 const personalDataSchema = defaultRegisterSchema.concat(fullRegisterSchema)
 
