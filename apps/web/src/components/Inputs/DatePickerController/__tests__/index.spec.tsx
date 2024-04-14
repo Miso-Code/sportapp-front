a33dbee25096859cd874c75d323e29687f5cf52b
Rodrigo Escobar Lopez <r.escobarl@uniandes.edu.co>
2024-04-14 16:56:33 -0500

    Feature: PSA-174 add sport data form and nutricional data form connections (#41)
    * feat: add new texts to translate forms PSA-174
    
    * feat: add robots.txt PSA-174
    
    * feat: update html PSA-174
    
    * feat: update datepicker PSA-173
    
    * feat: update lang and repository PSA-173
    
    * feat: update date picker component PSA-173
    
    * feat: update personal data form PSA-173
    
    * feat: update nutritional data form PSA-173
    
    * feat: update sport data form PSA-173
    
    * feat: add conections in home forms PSA-173
    
    * feat: update register full container PSA-173
    
    * feat: update register page PSA-173
    
    * feat: update login page PSA-173
    
    * feat: update router to change lang in html lang PSA-173
    
    * feat: correction PSA-173

--- apps/web/src/components/Inputs/DatePickerController/__tests__/index.spec.tsx
+++ apps/web/src/components/Inputs/DatePickerController/__tests__/index.spec.tsx
@@ -4,21 +4,27 @@ import { useForm } from 'react-hook-form'
 
 describe('DatePickerController', () => {
 	let wrapper: RenderResult
+	let spy: jest.SpyInstance
 
 	beforeEach(() => {
+		const mockedDate = new Date(1996, 6, 19)
+
+		spy = jest.spyOn(global, 'Date').mockImplementation(() => mockedDate)
 		const { result } = renderHook(() => useForm())
 		result.current.register('date')
-		result.current.setValue('date', '1996-07-20')
+		result.current.setValue('date', '1996-07-19')
 		wrapper = render(
 			<DatePickerController
 				control={result.current.control}
 				name='date'
 				label='Date'
+				value={new Date('1996-07-19')}
 			/>
 		)
 	})
 
 	afterEach(() => {
+		spy.mockRestore()
 		wrapper.unmount()
 	})
 
@@ -33,6 +39,7 @@ describe('DatePickerController', () => {
 				control={result.current.control}
 				name='date'
 				label='Date'
+				value={new Date('1996-07-19')}
 			/>
 		)
 		expect(wrapper.container).toMatchSnapshot()
@@ -49,6 +56,7 @@ describe('DatePickerController', () => {
 				control={result.current.control}
 				name='date'
 				label='Date'
+				value={new Date('1996-07-19')}
 			/>
 		)
 
--- apps/web/src/pages/Home/__tests__/index.spec.tsx
+++ apps/web/src/pages/Home/__tests__/index.spec.tsx
@@ -2,7 +2,7 @@ import { FormDataRequired as NutritionalFormData } from '@/containers/Nutritiona
 import { FormData as PersonalFormData } from '@/containers/PersonalDataForm/utils/schema'
 import { FormDataRequired as SportFormData } from '@/containers/SportDataForm/utils/schema'
 import { RenderResult, render } from '@testing-library/react'
-import Home from 'pages/Home'
+import HomePage from 'pages/Home'
 import { ReactNode, useState } from 'react'
 
 jest.mock(
@@ -16,29 +16,34 @@ jest.mock(
 			customSubmit: ReactNode
 			handleCustomSubmit: (data: NutritionalFormData) => void
 			inputsDisabled: boolean
-		}) => (
-			<div className='mocked-nutritional-data-form'>
-				{customSubmit}
-				{inputsDisabled}
-				<button
-					onClick={() => {
-						const payload: NutritionalFormData = {
-							allergyType: ['test'],
-							foodPreferences: 'test'
-						}
+		}) =>
+			(
+				<div className='mocked-nutritional-data-form'>
+					{customSubmit}
+					{inputsDisabled}
+					<button
+						onClick={() => {
+							const payload: NutritionalFormData = {
+								allergyType: ['test'],
+								foodPreferences: 'test'
+							}
 
-						handleCustomSubmit(payload)
-					}}>
-					NutritionalDataForm
-				</button>
-			</div>
-		)
+							handleCustomSubmit(payload)
+						}}>
+						NutritionalDataForm
+					</button>
+				</div>
+			)
 )
 
 jest.mock('@sportapp/stores/src/user', () => ({
 	useUserStore: jest.fn().mockReturnValue({
 		getProfile: jest.fn(),
 		updateProfile: jest.fn(),
+		getSport: jest.fn(),
+		updateSport: jest.fn(),
+		getNutrition: jest.fn(),
+		updateNutrition: jest.fn(),
 		user: {}
 	})
 }))
@@ -54,37 +59,38 @@ jest.mock(
 			customSubmit: ReactNode
 			handleCustomSubmit: (data: PersonalFormData) => void
 			inputsDisabled: boolean
-		}) => (
-			<div className='mocked-personal-data-form'>
-				{customSubmit}
-				{inputsDisabled}
-				<button
-					onClick={() => {
-						const payload: PersonalFormData = {
-							birthday: '2021-01-01',
-							documentNumber: '123456789',
-							documentType: 'RG',
-							email: 'test@test.com',
-							gender: 'M',
-							name: 'Test',
-							lastName: 'Test',
-							password: '123456',
-							nationality: {
-								country: 'Brazil',
-								city: 'São Paulo'
-							},
-							residence: {
-								country: 'Brazil',
-								city: 'São Paulo',
-								lengthOfStay: '1'
+		}) =>
+			(
+				<div className='mocked-personal-data-form'>
+					{customSubmit}
+					{inputsDisabled}
+					<button
+						onClick={() => {
+							const payload: PersonalFormData = {
+								birthday: new Date(),
+								documentNumber: '123456789',
+								documentType: 'RG',
+								email: 'test@test.com',
+								gender: 'M',
+								name: 'Test',
+								lastName: 'Test',
+								password: '123456',
+								nationality: {
+									country: 'Brazil',
+									city: 'São Paulo'
+								},
+								residence: {
+									country: 'Brazil',
+									city: 'São Paulo',
+									lengthOfStay: '1'
+								}
 							}
-						}
-						handleCustomSubmit(payload)
-					}}>
-					PersonalFormDataButton
-				</button>
-			</div>
-		)
+							handleCustomSubmit(payload)
+						}}>
+						PersonalFormDataButton
+					</button>
+				</div>
+			)
 )
 
 jest.mock(
@@ -98,27 +104,33 @@ jest.mock(
 			customSubmit: ReactNode
 			handleCustomSubmit: (data: SportFormData) => void
 			inputsDisabled: boolean
-		}) => (
-			<div className='mocked-sport-data-form'>
-				{customSubmit}
-				{inputsDisabled}
-				<button
-					onClick={() => {
-						const payload: SportFormData = {
-							availableTrainingHoursPerWeek: 1,
-							favouriteSportId: 1,
-							height: 1,
-							weight: 1,
-							limitations: ['test'],
-							trainingFrequency: '1',
-							trainingObjective: '1'
-						}
-						handleCustomSubmit(payload)
-					}}>
-					SportFormDataButton
-				</button>
-			</div>
-		)
+		}) =>
+			(
+				<div className='mocked-sport-data-form'>
+					{customSubmit}
+					{inputsDisabled}
+					<button
+						onClick={() => {
+							const payload: SportFormData = {
+								availableTrainingHoursPerWeek: 1,
+								favouriteSportId: '1',
+								height: 1,
+								weight: 1,
+								limitations: [
+									{
+										description: 'test',
+										name: 'test'
+									}
+								],
+								trainingFrequency: '1',
+								trainingObjective: '1'
+							}
+							handleCustomSubmit(payload)
+						}}>
+						SportFormDataButton
+					</button>
+				</div>
+			)
 )
 
 jest.mock('react', () => {
@@ -134,7 +146,7 @@ describe('Home', () => {
 	let wrapper: RenderResult
 
 	beforeEach(() => {
-		wrapper = render(<Home />)
+		wrapper = render(<HomePage />)
 	})
 
 	afterEach(() => {
@@ -147,19 +159,19 @@ describe('Home', () => {
 
 	it('should render PersonalDataForm', () => {
 		;(useState as jest.Mock).mockReturnValueOnce([0, jest.fn()])
-		wrapper.rerender(<Home />)
+		wrapper.rerender(<HomePage />)
 		expect(wrapper.getByText('PersonalFormDataButton')).toBeInTheDocument()
 	})
 
 	it('should render SportDataForm', () => {
 		;(useState as jest.Mock).mockReturnValueOnce([1, jest.fn()])
-		wrapper.rerender(<Home />)
+		wrapper.rerender(<HomePage />)
 		expect(wrapper.getByText('SportFormDataButton')).toBeInTheDocument()
 	})
 
 	it('should render NutritionalDataForm', () => {
 		;(useState as jest.Mock).mockReturnValueOnce([2, jest.fn()])
-		wrapper.rerender(<Home />)
+		wrapper.rerender(<HomePage />)
 		expect(wrapper.getByText('NutritionalDataForm')).toBeInTheDocument()
 	})
 
@@ -170,7 +182,7 @@ describe('Home', () => {
 			jest.fn()
 		])
 
-		wrapper.rerender(<Home />)
+		wrapper.rerender(<HomePage />)
 		wrapper.getByText('PersonalFormDataButton').click()
 		expect(wrapper.getByText('PersonalFormDataButton')).toBeInTheDocument()
 	})
@@ -182,7 +194,7 @@ describe('Home', () => {
 			jest.fn()
 		])
 
-		wrapper.rerender(<Home />)
+		wrapper.rerender(<HomePage />)
 		wrapper.getByText('SportFormDataButton').click()
 		expect(wrapper.getByText('SportFormDataButton')).toBeInTheDocument()
 	})
@@ -194,12 +206,12 @@ describe('Home', () => {
 			jest.fn()
 		])
 
-		wrapper.rerender(<Home />)
+		wrapper.rerender(<HomePage />)
 		wrapper.getByText('NutritionalDataForm').click()
 		expect(wrapper.getByText('NutritionalDataForm')).toBeInTheDocument()
 	})
 
-	it('should handle edit', () => {
+	it('should handle edit personalDataForm', () => {
 		const handleEdit = jest.fn()
 		;(useState as jest.Mock).mockReturnValueOnce([0, jest.fn()])
 		;(useState as jest.Mock).mockReturnValueOnce([
@@ -211,12 +223,12 @@ describe('Home', () => {
 			handleEdit
 		])
 
-		wrapper.rerender(<Home />)
+		wrapper.rerender(<HomePage />)
 		wrapper.getByText('personalDataForm.edit').click()
 		expect(handleEdit).toHaveBeenCalled()
 	})
 
-	it('should handle edit', () => {
+	it('should handle edit sportDataForm', () => {
 		const handleEdit = jest.fn()
 		;(useState as jest.Mock).mockReturnValueOnce([1, jest.fn()])
 		;(useState as jest.Mock).mockReturnValueOnce([
@@ -228,12 +240,12 @@ describe('Home', () => {
 			handleEdit
 		])
 
-		wrapper.rerender(<Home />)
+		wrapper.rerender(<HomePage />)
 		wrapper.getByText('sportDataForm.edit').click()
 		expect(handleEdit).toHaveBeenCalled()
 	})
 
-	it('should handle edit', () => {
+	it('should handle edit nutritionalDataForm', () => {
 		const handleEdit = jest.fn()
 		;(useState as jest.Mock).mockReturnValueOnce([2, jest.fn()])
 		;(useState as jest.Mock).mockReturnValueOnce([
@@ -245,7 +257,7 @@ describe('Home', () => {
 			handleEdit
 		])
 
-		wrapper.rerender(<Home />)
+		wrapper.rerender(<HomePage />)
 		wrapper.getByText('nutritionalDataForm.edit').click()
 		expect(handleEdit).toHaveBeenCalled()
 	})
