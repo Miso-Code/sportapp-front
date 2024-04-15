10589938d8f223b007aa230ce3f552cf22fc597a
ocralo <r.escobarl@uniandes.edu.co>
2024-04-14 20:11:55 -0500

    fix: correction langs and styles PSA-174

--- apps/web/src/containers/PersonalDataForm/__tests__/index.spec.tsx
+++ apps/web/src/containers/PersonalDataForm/__tests__/index.spec.tsx
@@ -3,12 +3,18 @@ import { render, RenderResult } from '@testing-library/react'
 
 describe('PersonalDataForm', () => {
 	let wrapper: RenderResult
+	let spy: jest.SpyInstance
 
 	beforeEach(() => {
+		const mockedDate = new Date(1996, 6, 19)
+
+		spy = jest.spyOn(global, 'Date').mockImplementation(() => mockedDate)
+
 		wrapper = render(<PersonalDataForm handleCustomSubmit={jest.fn()} />)
 	})
 
 	afterEach(() => {
+		spy.mockRestore()
 		wrapper.unmount()
 	})
 
