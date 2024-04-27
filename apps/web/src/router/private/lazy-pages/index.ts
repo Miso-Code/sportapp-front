882559fad58ad0d63cc3f1cce893614aa8575748
ocralo <r.escobarl@uniandes.edu.co>
2024-04-26 23:05:16 -0500

    feat: update format PSA-202

--- apps/web/src/router/private/lazy-pages/index.ts
+++ apps/web/src/router/private/lazy-pages/index.ts
@@ -5,6 +5,12 @@ export const ConfigLazy = lazy(() => import('@/pages/User/Config'))
 
 // partner pages
 export const HomePartnerLazy = lazy(() => import('@/pages/Partner/Home'))
-export const CreateProductPartnerLazy = lazy(() => import('@/pages/Partner/CreateProduct'))
-export const PurchasedProductPartnerLazy = lazy(() => import('@/pages/Partner/PurchasedProduct'))
-export const UpdateProductPartnerLazy = lazy(() => import('@/pages/Partner/UpdateProduct'))
+export const CreateProductPartnerLazy = lazy(
+	() => import('@/pages/Partner/CreateProduct')
+)
+export const PurchasedProductPartnerLazy = lazy(
+	() => import('@/pages/Partner/PurchasedProduct')
+)
+export const UpdateProductPartnerLazy = lazy(
+	() => import('@/pages/Partner/UpdateProduct')
+)
