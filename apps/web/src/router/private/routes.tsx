cca1785e10ea61946d225e5f379990b695024d06
ocralo <r.escobarl@uniandes.edu.co>
2024-04-28 14:34:54 -0500

    feat: add preference page PSA-203

--- apps/web/src/router/private/routes.tsx
+++ apps/web/src/router/private/routes.tsx
@@ -1,6 +1,13 @@
 import Layout from '@/router/private/Layout'
 import { Navigate } from 'react-router-dom'
-import { ConfigLazy, HomeLazy } from '@/router/private/lazy-pages'
+import {
+	ConfigLazy,
+	HomeLazy,
+	ListSchedulePreferencePageLazy,
+	PreferenceCardSelectionPageLazy,
+	PreferenceFormPageLazy,
+	PreferencePageLazy
+} from '@/router/private/lazy-pages'
 
 export default function privateRoutesUser() {
 	return {
@@ -8,6 +15,16 @@ export default function privateRoutesUser() {
 		children: [
 			{ path: '/home', element: <HomeLazy /> },
 			{ path: '/config', element: <ConfigLazy /> },
+			{ path: '/preferences', element: <PreferencePageLazy /> },
+			{
+				path: '/preferences/selection',
+				element: <PreferenceCardSelectionPageLazy />
+			},
+			{ path: '/preferences/form', element: <PreferenceFormPageLazy /> },
+			{
+				path: '/preferences/list-schedule',
+				element: <ListSchedulePreferencePageLazy />
+			},
 			{ path: '*', element: <Navigate to='/' replace /> }
 		]
 	}
